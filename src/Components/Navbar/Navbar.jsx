988cbf7783ccdd46f293a85592ee636b572be53e
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-08 15:29:24 -0400

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './navbar.scss'
 import { MdOutlineTravelExplore } from 'react-icons/md'
+import { AiFillCloseCircle } from "react-icons/ai"
 
 const Navbar = () => {
   return (
@@ -12,6 +13,38 @@ const Navbar = () => {
             <h1><MdOutlineTravelExplore className="icon" /> Travel.</h1>
           </a>
         </div>
+
+        <div className="navBar">
+          <ul className="navLists flex">
+            <li className="navItem">
+              <a href="" className="navLink">Home</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Packages</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Shop</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Pages</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">News</a>
+            </li>
+            <li className="navItem">
+              <a href="" className="navLink">Contact</a>
+            </li>
+            <button className="btn">
+              <a href="#">Book now</a>
+            </button>
+          </ul>
+
+          <div className="closeNavbar">
+            <AiFillCloseCircle className="icon" />
+          </div>
+        </div>
+
+
       </header>
     </section>
   )
