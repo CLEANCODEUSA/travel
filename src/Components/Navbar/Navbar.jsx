a055916fb336af2b0a0f468ec419352ad82454b9
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-08 15:22:09 -0400

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import './navbar.scss'
+import { MdOutlineTravelExplore } from 'react-icons/md'
 
 const Navbar = () => {
   return (
     <section className='navBarSection'>
+      <header className="header flex">
 
+        <div className="logoDiv">
+          <a href="" className="logo flex">
+            <h1><MdOutlineTravelExplore className="icon" /> Travel.</h1>
+          </a>
+        </div>
+      </header>
     </section>
   )
 }
