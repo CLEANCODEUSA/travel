e5930e153f80f3706a3173ad8c7653d5c8b7b84a
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-08 23:10:46 -0400

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,7 +3,20 @@ import './home.scss'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="overlay"></div>
+      <video src="" muted autoPlay loop type="video/mp4"></video>
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallTexst">
+            Our Packages
+          </span>
+          <h1 className="homeTitle">
+            Search your Holiday
+          </h1>
+        </div>
+      </div>
+    </section>
   )
 }
 
