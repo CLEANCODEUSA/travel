3f6464f74ef3567f7b5bda522d4a2155f28617d0
Vishal M <48333506+khs56@users.noreply.github.com>
2024-05-08 23:10:44 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ import Footer from './Components/Footer/Footer'
 
 const App = () => {
   return (
-    // <h2>Working</h2>
     <>
       <Navbar />
-      {/* <Home />
-      <Main />
+      <Home />
+      {/* <Main />
       <Footer /> */}
     </>
   )
